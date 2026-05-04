da7e5c4da3100ecd1c47625a1e9b832f340ca9bf
yash6564garg-maker <yash6564garg@gmail.com>
2026-05-04 22:45:16 +0530

    Delete ygdigitals_website_source directory

--- ygdigitals_website_source/src/components/ui/button-colorful-demo.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonColorful } from "@/components/ui/button-colorful"
-import { Button } from "@/components/ui/button"
-import { LucideIcon } from "lucide-react"
-
-function ButtonDemo() {
-    return <ButtonColorful />
-}
-
-export { ButtonDemo }
--- ygdigitals_website_source/src/components/ui/pricing-component-demo.tsx
+++
@@ -1,5 +0,0 @@
-import Pricing from "@/components/ui/pricing-component";
-
-export default function PricingComponentDemo() {
-  return <Pricing />;
-}
